5b1cfecd58031cfe2e8be57a3a0117fa76e99645
Adam Brown <adamwbrown@gmail.com>
2020-03-10 12:13:45 -0700

    Explain why we need the runBuild scripts

--- runBuild.bat
+++ runBuild.bat
@@ -1,1 +1,7 @@
+@echo off
+REM Currently having trouble passing arguments to the second command
+REM in Godot, thus this file acts as a way to pass arguments to gradle
+REM from Godot.
+REM If the argument passing problem can be solved in Godot, then these
+REM scripts can be removed.
 call gradlew.bat build
